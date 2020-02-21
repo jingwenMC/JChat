c535e71706edee335d6a07e8a79def88dc26e844
jingwenMC <2313055824@qq.com>
2020-02-21 10:15:14 +0800

    Recode the sender

--- src/main/java/cn/jplex/jchat/Jchat.java
+++ src/main/java/cn/jplex/jchat/Jchat.java
@@ -13,11 +13,15 @@ public final class Jchat extends JavaPlugin {
 
     @Override
     public void onEnable() {
+        this.getServer().getMessenger().registerOutgoingPluginChannel(this, "BungeeCord");
+        this.getServer().getMessenger().registerIncomingPluginChannel(this, "BungeeCord", new BungeeListener());
+        System.out.println("=====================================");
         System.out.println("=====JChat By JPlex Develop Team=====");
         System.out.println("=====Developed for JPlex Network=====");
         System.out.println("=====License:Open-Source,GPL3.0 =====");
-        System.out.println("=====Version:PRE-0.1-SNAPSHOT   =====");
+        System.out.println("=====Version:PRE-0.2-SNAPSHOT   =====");
         System.out.println("=====Now Enabled.  (c)JPlex 2020=====");
+        System.out.println("=====================================");
         // Plugin startup logic
         //MySQLDatabase conn = new MySQLDatabase();
         getCommand("lb").setExecutor(new lb());
@@ -26,11 +30,13 @@ public final class Jchat extends JavaPlugin {
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+        System.out.println("=====================================");
         System.out.println("=====JChat By JPlex Develop Team=====");
         System.out.println("=====Developed for JPlex Network=====");
         System.out.println("=====License:Open-Source,GPL3.0 =====");
-        System.out.println("=====Version:PRE-0.1-SNAPSHOT   =====");
+        System.out.println("=====Version:PRE-0.2-SNAPSHOT   =====");
         System.out.println("=====Now Disabled. (c)JPlex 2020=====");
         System.out.println("=====Goodbye!                   =====");
+        System.out.println("=====================================");
     }
 }
--- src/main/java/cn/jplex/jchat/commands/lb.java
+++ src/main/java/cn/jplex/jchat/commands/lb.java
@@ -1,42 +1,30 @@
 package cn.jplex.jchat.commands;
 
-import com.google.common.collect.Iterables;
 import com.google.common.io.ByteArrayDataOutput;
 import com.google.common.io.ByteStreams;
-import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
+import cn.jplex.jchat.BungeeListener;
 //import gyurix.api.BungeeAPI;
 
 
 public class lb implements CommandExecutor {
+    private BungeeListener bungeeListener = new BungeeListener();
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        //if (sender instanceof Player)
-        //{
-            //Player player = (Player) sender;
-        ByteArrayDataOutput out = ByteStreams.newDataOutput();
-        out.writeUTF("Message");
-        out.writeUTF("ALL");
-        out.writeUTF("HelloWorld");
-
-        // If you don't care about the player
-        Player player = Iterables.getFirst(Bukkit.getOnlinePlayers(), null);
-        // Else, specify them
-        //Player player = Bukkit.getPlayerExact("Example");
-
-        player.sendPluginMessage((Plugin) this, "BungeeCord", out.toByteArray());
-            //command here
-            //gyurix.api.BungeeAPI.sendMessage("HelloWorld",gyurix.api.BungeeAPI.totalPlayerList());
-        //}
-        //else
-       // {
-           // System.out.println(ChatColor.RED +"请由一名玩家执行");
-       // }
+        if (sender instanceof Player)
+        {
+            Player player = (Player) sender;
+            bungeeListener.sendBungeeMeesage("ALL","HelloWorld");
+        }
+        else
+        {
+            System.out.println(ChatColor.RED +"请由一名玩家执行");
+        }
         return false;
     }
 }
